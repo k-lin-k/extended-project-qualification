253b6983598df92c339bc4749199ed31a4890620
ku-storage <wellokayemmalin@gmail.com>
2023-01-30 14:25:47 +0000

    update

--- static/script.js
+++ static/script.js
@@ -13,19 +13,22 @@ function drop(ev) {
 }
 
 var elem = document.getElementById(data);
-elem.onkeydown = function(elem){
-    if(elem.keyCode == 13){
+
+function enter(){
+    if(event.keyCode == 13){
        validate();
     }
+
 }
 
 function validate(elem) {
     if(elem == "Lovelace"){
         location.href="https://support.wwf.org.uk/earth_hour/index.php?type=individual";
+    } else if(elem == "Hopper"){
+
+    } else if(elem == "WWII"){
+
+    } else {
+        alertEmpty()
     }
 }
-
-<a href="/extended-project-qualification/Lovelace"></a>
-<a href="/extended-project-qualification/Hopper"></a>
-<a href="/extended-project-qualification/WWII"></a>
-<a href="/extended-project-qualification/CS"></a>
