b1c2e8c33ebe846d01eb8198e5846932cb5d2501
ku-storage <wellokayemmalin@gmail.com>
2023-01-31 10:34:23 +0000

    YASSSS

--- static/script.js
+++ static/script.js
@@ -3,21 +3,16 @@ function allowDrop(ev) {
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+    var item = ev.target.id;
+    var html = ".html";
+    var link = item + html;
+  ev.dataTransfer.setData("link", link);
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    var elem = document.getElementById(data);
+    window.location=ev.dataTransfer.getData("link");
     ev.target.appendChild(document.getElementById(data));
-    if(ev.target.id == "Lovelace"){
-        window.location.href="/Lovelace.html";
-    } else if(ev.target.id == "Hopper"){
-        window.location.href="Hopper.html";
-    } else if(ev.target.id == "WWII"){
-        window.location.href="WWII.html";
-    } else {
-        alertempty()
-  }
+
 }
